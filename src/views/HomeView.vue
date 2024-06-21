1e2add065d6a48b1789789e3552f24b6bf485747
qiaozhididi <1343013603@qq.com>
2024-06-21 22:42:59 +0800

    初始化项目

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>首页</div>
 </template>
+
+<script setup></script>
+<style scoped lang="ts"></style>
