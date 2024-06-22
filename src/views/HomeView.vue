99676247039095597abf1efaa8172fe77fdcaf60
qiaozhididi <1343013603@qq.com>
2024-06-22 20:27:18 +0800

    Props 标注类型 setup场景

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,24 @@
 
   <ComputedDemo />
   <EventDemo />
+  <PropsDemo title="Props传参" text="123123" :state="state" />
 </template>
 
 <script setup lang="ts">
+import { reactive } from "vue";
 import RefDemo from "../components/RefDemo.vue";
 import ReactiveDemo from "@/components/ReactiveDemo.vue";
 import ComputedDemo from "@/components/ComputedDemo.vue";
 import EventDemo from "@/components/EventDemo.vue";
+import PropsDemo from "@/components/PropsDemo.vue";
+
+interface iState {
+  name: string;
+  age: number;
+}
+const state: iState = reactive({
+  name: "王五",
+  age: 28,
+});
 </script>
 <style scoped></style>
