015489d1315dc9e68b8a4b6f0bc2d73c677b4010
qiaozhididi <1343013603@qq.com>
2024-06-22 14:07:02 +0800

    computed计算属性标注类型的使用

---
+++ src/components/ComputedDemo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="computedDemo">
+    <h3>Computed</h3>
+    <div>count1: {{ doubleCount }}</div>
+    <div>count2: {{ doubleCount2 }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from "vue";
+const count = ref<number>(100);
+//默认推导为number类型
+const doubleCount = computed(() => count.value * 2);
+
+// const result = doubleCount.value.split("0") //报错 值类型不能被拆分
+
+//指定了类型 返回值必须是number类型
+const doubleCount2 = computed<number>(() => {
+  return count.value * 2;
+});
+</script>
+<style scoped>
+#computedDemo {
+  width: 100%;
+  border: 1px solid yellow;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,13 @@
   <RefDemo />
 
   <ReactiveDemo />
+
+  <ComputedDemo />
 </template>
 
 <script setup lang="ts">
 import RefDemo from "../components/RefDemo.vue";
 import ReactiveDemo from "@/components/ReactiveDemo.vue";
+import ComputedDemo from "@/components/computedDemo.vue";
 </script>
 <style scoped></style>
